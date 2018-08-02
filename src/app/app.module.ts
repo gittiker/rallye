860d7ebe14e5fc6b3956f681ec6afb4f3ef26878
Marco Faulhaber <mail@marco-faulhaber.de>
2018-08-02 22:35:53 +0200

    CSS Anpassungen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,3 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import { AgmCoreModule } from '@agm/core';
-
-// import { AppComponent } from './app.component';
-// import { MapComponent } from './map/map.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     MapComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AgmCoreModule.forRoot({
-//       apiKey: 'AIzaSyAC5kT6DWJ2io2vsw6ZRS9-7kLy5g2xJmQ'
-//     })
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
