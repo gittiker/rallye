2ef2aa08092004f1441208b8a94df3c17334d9db
Marco <you@example.com>
2018-08-06 12:03:30 +0200

    Init Database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
-
+import * as firebase from "firebase"
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+constructor(){
+
+  firebase.initializeApp(firebaseConfig)
+
+  firebase.database().ref("/").once("value", data=>{
+    let dataa = data.exportVal()
+    console.log(dataa.groups)
+    console.log(data.exportVal())
+  })
 }
+
+}
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQ_zgFYn4O8pTmvcNPJRCJkK8Vq9bOwo8",
+  authDomain: "bgltour-b529d.firebaseapp.com",
+  databaseURL: "https://bgltour-b529d.firebaseio.com",
+  projectId: "bgltour-b529d",
+  storageBucket: "bgltour-b529d.appspot.com",
+  messagingSenderId: "744135181738"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import * as Firebase from 'firebase';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AgmCoreModule } from '@agm/core';
@@ -14,7 +16,7 @@ import { MapComponent } from './map/map.component';
     AgmCoreModule.forRoot({
       // please get your own API key here:
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
-      apiKey: 'AIzaSyB31tnVHuobHgbwb6uHVoky299RonHCEyo'
+      // apiKey: 'AIzaSyB31tnVHuobHgbwb6uHVoky299RonHCEyo'
       // apiKey: 'AIzaSyDWLloWmmnM74Fvrlm7_EcoKzMrW4Fxhxo' //Maps V2
     })
   ],
