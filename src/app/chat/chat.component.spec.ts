62c55aae4bbd4c5713ad377e3b5ba270e3985304
Rappenhoener <drappenhoener@gambit.de>
2018-08-07 12:19:25 +0200

    Chat in Container eingefuegt

--- src/app/chat/chat.component.spec.ts
+++ src/app/chat/chat.component.spec.ts
@@ -1,4 +1,4 @@
-/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChatComponent } from './chat.component';
 
@@ -23,4 +23,3 @@ describe('ChatComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-*/
