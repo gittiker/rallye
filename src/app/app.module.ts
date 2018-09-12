1e7ab1ccee6e2c0fe034a5a873ca74d9c48d3b17
Marco <you@example.com>
2018-09-12 20:22:01 +0200

    deleted code of other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { AgmCoreModule } from '@agm/core';
-import { MapComponent } from './map/map.component';
 import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule,
-    AgmCoreModule.forRoot({
-      // please get your own API key here:
-      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
-      apiKey: 'AIzaSyB31tnVHuobHgbwb6uHVoky299RonHCEyo'
-      // apiKey: 'AIzaSyDWLloWmmnM74Fvrlm7_EcoKzMrW4Fxhxo' //Maps V2
-    })
+    FormsModule
   ],
-  declarations: [ AppComponent, HelloComponent, MapComponent, ChatComponent ],
+  declarations: [ AppComponent, ChatComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
