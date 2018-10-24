1badca72dc2adbeb6174517512830ed03d9b89dd
Marco <you@example.com>
2018-10-24 20:42:35 +0200

    adjustments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
+  
+  firebase: {
+    apiKey: "AIzaSyBQ_zgFYn4O8pTmvcNPJRCJkK8Vq9bOwo8",
+    authDomain: "bgltour-b529d.firebaseapp.com",
+    databaseURL: "https://bgltour-b529d.firebaseio.com",
+    projectId: "bgltour-b529d",
+    storageBucket: "bgltour-b529d.appspot.com",
+    messagingSenderId: "744135181738"
+  },
 
-  // firebase: {
-  //   apiKey: "AIzaSyBQ_zgFYn4O8pTmvcNPJRCJkK8Vq9bOwo8",
-  //   authDomain: "bgltour-b529d.firebaseapp.com",
-  //   databaseURL: "https://bgltour-b529d.firebaseio.com",
-  //   projectId: "bgltour-b529d",
-  //   storageBucket: "bgltour-b529d.appspot.com",
-  //   messagingSenderId: "744135181738"
-  // },
-
-  // mapbox: {
-  //   accessToken: 'pk.eyJ1IjoibWFwbm9vYiIsImEiOiJjamtrMjBrazQxbHByM3ZueGp1Y3pkaWZxIn0.q5ce20FZh7u2tOVBxsomFA'
-  // }
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWFwbm9vYiIsImEiOiJjamtrMjBrazQxbHByM3ZueGp1Y3pkaWZxIn0.q5ce20FZh7u2tOVBxsomFA'
+  }
 };
